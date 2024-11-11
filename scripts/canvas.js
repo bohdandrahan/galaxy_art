767d420a3977babdc91f868f0e5841df2ee64d3a
bohdandrahan <bohdan.drahan@gmail.com>
2024-11-10 21:25:37 -0500

    looks like something

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,11 @@
-let smaller_window_size;
 let canvas_scale_x = 0.85;
 let canvas_scale_y = 0.8;
 let windows_width;
 let windows_height;
 let pixel_fracture = 1000; // divide smaller window size on this var to get one unit of size;
+
+let space_elements;
+let space_black_elements;
 
 function preload() {
     font = loadFont('./assets/fonts/Asap-Regular.otf');
@@ -22,31 +24,69 @@ function setup() {
     background(200);
 
     cam = createCamera();
-    cam.setPosition(300, 400, 550);
+    cam.setPosition(5000, 400, 550);
     cam.lookAt(0, 0, 0)
 
     orbitControl(1, 1, 1);
 
     space_elements = [];
-    for (let i = 0; i < 10; i++) {
-        space_element = create_element();
-        space_elements.push(1);
+    for (let i = 0; i < 100; i++) {
+        let element_color = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
+        let space_element = new SpaceElement(500, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, 1,
+            10, element_color, 25, 1)
+
+        space_elements.push(space_element);
+    }
+
+    space_black_elements = []
+    for (let i = 0; i < 200; i++) {
+        let element_color = [0, 0, 0]
+        let space_element = new SpaceElement(1500, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, 1,
+            10, element_color, 25, 1)
+
+        space_black_elements.push(space_element);
+    }
+
+    space_black_elements = []
+    for (let i = 0; i < 200; i++) {
+        let element_color = [0, 0, 0]
+        let space_element = new SpaceElement(1500, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, 1,
+            10, element_color, 25, 1)
+
+        space_black_elements.push(space_element);
     }
     console.log(space_elements)
 }
 
 function draw() {
-    background(200)
+    //background(200)
     normalMaterial();
-    box()
     orbitControl(1, 1, 1);
-    draw_debug();
+
+    space_black_elements.forEach((element, index) => {
+        draw_element(element)
+
+        //element.update_coordinates()
+
+    })
+    space_elements.forEach((element, index) => {
+        draw_element(element)
+
+        //element.update_coordinates()
+
+    })
+    //draw_debug();
 }
 
-function create_element() {
-    //radial distance,  polar angle, azimuthal angle, element_radius, element_color
-    return
+function draw_element(element) {
+    coordinates = element.get_xyz()
+
+    translate(...coordinates)
+    fill(...element.color)
+    sphere(element.element_radius)
+    translate(-coordinates[0], -coordinates[1], -coordinates[2])
 }
+
 
 function draw_debug() {
 
--- scripts/element.js
+++ scripts/element.js
@@ -5,17 +5,25 @@
 // 
 // the element should support the pulsing effect, meaning the sphere will "breathe" by changing the diameter in sin function with the amplitude change set in percentage from the radius, and the frequency set in with the number called Period in a similar way to what I did in birdhouse project.
 
-class Element {
+class SpaceElement {
     constructor(radial_distance, polar_angle, azimuthal_angle, orbit_speed_period,
         element_radius, element_color, pulse_amplitude_change = 0, pulse_period = 1) {
+
         this.radial_distance = radial_distance;
         this.polar_angle = polar_angle;
-        this.azimuthal_angl = azimuthal_angle;
+        this.azimuthal_angle = azimuthal_angle;
         this.element_radius = element_radius;
         this.orbit_speed_period = orbit_speed_period;
-        this.element_color = element_color;
+        this.color = element_color;
         this.pulse_amplitude_change = pulse_amplitude_change
         this.pulse_period = pulse_period;
     }
 
+    get_xyz() {
+        let x = this.radial_distance * Math.sin(this.polar_angle) * Math.cos(this.azimuthal_angle)
+        let y = this.radial_distance * Math.sin(this.polar_angle) * Math.sin(this.azimuthal_angle)
+        let z = this.radial_distance * Math.cos(this.polar_angle)
+
+        return [x, y, z]
+    }
 }
